8c917381331935459dabb5f3aecb6092f6874360
AlexandrSeleznyov <vuavso16@gmail.om>
2021-08-31 17:42:23 +0300

    1

---
+++ src/components/FriendList/friendlistitem.js
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+import s from "./FriendList.module.css";
+export default function FriendListItem({avatar, name, isOnline}){
+    return (
+
+        <li className = {s.item}>
+        <span className = {s.status} style = {{backgroundColor : isOnline ? "green" : "red" }}></span>
+        <img src={avatar} alt={name} width="48" />
+        <p>{name}</p>
+      </li>
+    )
+}
+FriendListItem.propTypes = {
+    name:PropTypes.string.isRequired,
+    avatar:PropTypes.string.isRequired,
+    isOnline:PropTypes.bool.isRequired,
+     };
